c6c6fbfaa568196ff50c1add3ecabe63a7e9eb77
s2donald <s2donald@edu.uwaterloo.ca>
2023-11-22 23:56:59 -0500

    First commit

---
+++ src/vockal/recorder/listener.py
@@ -0,0 +1,30 @@
+from pynput import keyboard
+
+class Listener(keyboard.Listener):
+    def __init__(self, recorder, player):
+        super().__init__(on_press=self.on_press, on_release=self.on_release)
+        self.recorder = recorder
+        self.player = player
+
+    def on_press(self, key):
+        if key is None:  # unknown event
+            pass
+        elif isinstance(key, keyboard.Key):  # special key event
+            if key.ctrl and self.player.playing == 0:
+                self.recorder.start()
+        elif isinstance(key, keyboard.KeyCode):  # alphanumeric key event
+            if key.char == 'q':  # press q to quit
+                if self.recorder.recording:
+                    self.recorder.stop()
+                return False  # this is how you stop the listener thread
+            if key.char == 'p' and not self.recorder.recording:
+                self.player.start()
+
+    def on_release(self, key):
+        if key is None:  # unknown event
+            pass
+        elif isinstance(key, keyboard.Key):  # special key event
+            if key.ctrl:
+                self.recorder.stop()
+        elif isinstance(key, keyboard.KeyCode):  # alphanumeric key event
+            pass
