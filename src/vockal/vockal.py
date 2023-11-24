66a78ef16174398f312e0fdc474bddd531a3285e
s2donald <s2donald@uwaterloo.ca>
2023-11-24 17:08:37 -0500

    Incorporated some changes to make the program run

--- src/vockal/vockal.py
+++ src/vockal/vockal.py
@@ -5,7 +5,6 @@ from src.vockal.utils import utils as utl
 from src.vockal.recorder.listener import Listener
 from src.vockal.recorder.player import Player
 from src.vockal.recorder.record import Recorder
-import soundfile as sf
 
 from vockal.utils.constants import SOUNDFILE
 
@@ -15,8 +14,8 @@ class VockalMain():
         pass
 
     def run(self):
-        r = Recorder(SOUNDFILE)
-        p = Player(SOUNDFILE)
+        r = Recorder("mic.wav")
+        p = Player("mic.wav")
         l = Listener(r, p)
         print('Hold ctrl to record, Press p to playback, Press q to quit')
         l.start()  # keyboard listener is a thread so we start it here
@@ -24,19 +23,13 @@ class VockalMain():
 
         transcription = self.__transcribe_audio()
         print(transcription)
+        self.__remove_temp_audio()
         return
 
     @staticmethod
-    def __print_sound_details(self):
-        f = sf.SoundFile(SOUNDFILE)
-        print('samples = {}'.format(f.frames))
-        print('sample rate = {}'.format(f.samplerate))
-        print('seconds = {}'.format(f.frames / f.samplerate))
-
-    @staticmethod
-    def __transcribe_audio(self):
+    def __transcribe_audio():
         return utl.transcribe_audio()
 
     @staticmethod
-    def __remove_temp_audio(self):
+    def __remove_temp_audio():
         os.remove(SOUNDFILE)
