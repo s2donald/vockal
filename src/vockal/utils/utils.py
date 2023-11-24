e947d1ae4ac76d644864034d08042e489369c4a2
s2donald <s2donald@uwaterloo.ca>
2023-11-24 14:12:10 -0500

    Added some changes

--- src/vockal/utils/utils.py
+++ src/vockal/utils/utils.py
@@ -1,25 +1,9 @@
-import sounddevice as sd
-import tempfile
-import soundfile as sf
-import openai
-import os
+import speech_recognition as sr
+from vockal.utils.constants import SOUNDFILE
 
-
-def record_audio(duration, fs, channels):
-    recording = sd.rec(int(duration * fs), samplerate=fs, channels=channels)
-    sd.wait()
-    return recording
-
-def transcribe_audio(recording, fs):
-    with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as temp_audio:
-        sf.write(temp_audio.name, recording, fs)
-        temp_audio.close()
-        with open(temp_audio.name, "rb") as audio_file:
-            transcript = openai.Audio.transcribe("whisper-1", audio_file)
-        os.remove(temp_audio.name)
-    return transcript["text"].strip()
-
-def play_generated_audio(text, voice="", model=""):
-    # audio = generate(text=text, voice=voice, model=model)
-    # play(audio)
-    pass
+def transcribe_audio():
+    r = sr.Recognizer()
+    with sr.AudioFile(SOUNDFILE) as source:
+        audio = r.record(source)  # read the entire audio file
+        transcription = r.recognize_google(audio)
+    return transcription
